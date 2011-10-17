e55fb1e1c257a993c63e0415d247f73cbada8386
Robert T. Goeddel <rgoeddel@umich.edu>
2011-10-17 10:31:54 -0400

    Removed some code :(

--- ps2/eecs568/java/src/april/sim/LeastSquaresListener.java
+++ ps2/eecs568/java/src/april/sim/LeastSquaresListener.java
@@ -13,10 +13,6 @@ public class LeastSquaresListener implements Simulator.Listener
     VisWorld vw;
     Config config;
 
-    double xyt[] = new double[3]; // Current rigid body transform
-
-    ArrayList<double[]> trajectory = new ArrayList<double[]>(); // Estimated trajectory
-    HashMap<Integer, Integer> lmark2state = new HashMap<Integer, Integer>();
     double baseline; // Robot baseline in [m]
 
     public void init(Config config_, VisWorld vw_)
@@ -29,13 +25,7 @@ public class LeastSquaresListener implements Simulator.Listener
 
     public void update(Simulator.odometry_t odom, ArrayList<Simulator.landmark_t> dets)
     {
-        // XXX Do our magic here
-        // Look through our landmarks. If we haven't seen them before, add
-        // them to our state vector and record their position
-        for (Simulator.landmark_t lmark: dets) {
-            if (!lmark2state.containsKey(lmark.id))
-                lmark2state.put(lmark.id, 0); // XXX For now, just drop an entry in
-        }
+        // Magic stuff here
 
         // Draw our trajectory and detections
         drawStuff();
