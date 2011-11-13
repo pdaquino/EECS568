67fe8bbc42793707f9877bf25bb778df27641601
pdaquino <pdaquino@umich.edu>
2011-11-13 16:34:54 -0500

    RANSAC code.

--- ps3/eecs568/java/src/team/Task2.java
+++ ps3/eecs568/java/src/team/Task2.java
@@ -10,70 +10,58 @@ import april.vis.*;
 import april.util.*;
 import april.lcmtypes.*;
 import april.jmat.*;
-import april.jmat.geom.*;
 
 import lcm.lcm.*;
 
-
-public class Task2 implements LCMSubscriber, ParameterListener
-{
+public class Task2 implements LCMSubscriber, ParameterListener {
 
     static LCM lcm = LCM.getSingleton();
-
     JFrame jf = new JFrame("Task2");
-
     VisWorld vw = new VisWorld();
     VisLayer vl = new VisLayer(vw);
     VisCanvas vc = new VisCanvas(vl);
-
     ParameterGUI pg = new ParameterGUI();
-
     laser_t laser; // synchronize on 'this'
     ArrayList<Line> lines = new ArrayList<Line>();
 
-    public Task2()
-    {
-        pg.addDoubleSlider("thresh","Thresh",0,1,.05);
+    public Task2() {
+        pg.addDoubleSlider("thresh", "Thresh", 0, 1, .03);
         pg.addInt("maxsteps", "Max Agglomeration Steps", 200);
-
+        pg.addListener(this);
+        
         jf.setLayout(new BorderLayout());
         jf.add(vc, BorderLayout.CENTER);
         jf.add(pg, BorderLayout.SOUTH);
 
-        jf.setSize(800,600);
+        jf.setSize(800, 600);
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        lcm.subscribe("LIDAR_FRONT",this);
-        lcm.subscribe("POSE",this);
+        lcm.subscribe("LIDAR_FRONT", this);
+        lcm.subscribe("POSE", this);
 
     }
 
-    public static class Line
-    {
-        ArrayList<double[]> line;
+    public static class Line {
 
+        ArrayList<double[]> line;
         // Line fit
         double[] q;
         double theta;
-
         // Moments
         double Mx, My, Mxx, Mxy, Myy;
 
-        public Line(ArrayList<double[]> line_)
-        {
+        public Line(ArrayList<double[]> line_) {
             line = line_;
             computeLineFit();
         }
 
-        public int size()
-        {
+        public int size() {
             return line.size();
         }
 
-        private void computeLineFit()
-        {
-            assert(line != null && line.size() > 0);
+        private void computeLineFit() {
+            assert (line != null && line.size() > 0);
 
             Mx = 0;
             My = 0;
@@ -84,92 +72,92 @@ public class Task2 implements LCMSubscriber, ParameterListener
                 double[] xy = line.get(i);
                 Mx += xy[0];
                 My += xy[1];
-                Mxx += xy[0]*xy[0];
-                Myy += xy[1]*xy[1];
-                Mxy += xy[0]*xy[1];
+                Mxx += xy[0] * xy[0];
+                Myy += xy[1] * xy[1];
+                Mxy += xy[0] * xy[1];
             }
 
             // Can optimize later XXX
-            double Cxx = (Mxx/line.size()) - ((Mx*Mx)/(line.size()*line.size()));
-            double Cyy = (Myy/line.size()) - ((My*My)/(line.size()*line.size()));
-            double Cxy = (Mxy/line.size()) - ((Mx*My)/(line.size()*line.size()));
+            double Cxx = (Mxx / line.size()) - ((Mx * Mx) / (line.size() * line.size()));
+            double Cyy = (Myy / line.size()) - ((My * My) / (line.size() * line.size()));
+            double Cxy = (Mxy / line.size()) - ((Mx * My) / (line.size() * line.size()));
 
             //System.out.printf("%f %f %f %f %f === %f %f %f\n", Mx, My, Mxx, Myy, Mxy, Cxx, Cyy, Cxy);
 
-            q = new double[] {Mx/line.size(), My/line.size()};
+            q = new double[]{Mx / line.size(), My / line.size()};
             //LinAlg.print(q);
-            theta = MathUtil.mod2pi(Math.PI/2 + MathUtil.atan2(-2*Cxy, Cyy-Cxx)/2);
+            theta = MathUtil.mod2pi(Math.PI / 2 + MathUtil.atan2(-2 * Cxy, Cyy - Cxx) / 2);
             //System.out.printf("theta: %f\n", theta);
         }
 
         // If the merged line is below our threshold, keep it. Otherwise, return null
         // Assumes Line l will share its first point with your last
-        public Line tryLineMerge(Line l, double threshold)
-        {
+        public Line tryLineMerge(Line l, double threshold) {
             // Check for adjacency
-            assert(Arrays.equals(line.get(line.size()-1), l.line.get(0)));
+            assert (Arrays.equals(line.get(line.size() - 1), l.line.get(0)));
 
             ArrayList<double[]> points = new ArrayList<double[]>();
             points.addAll(line);
-            points.remove(points.size()-1);
+            points.remove(points.size() - 1);
             points.addAll(l.line);
             Line temp = new Line(points);
 
-            if (temp.getError() <= threshold)
+            if (temp.getError() <= threshold) {
                 return temp;
-            else
+            } else {
                 return null;
+            }
         }
 
-        public double getError()
-        {
+        public double getError() {
             double ct = Math.cos(theta);
             double st = Math.sin(theta);
 
-            return ct*ct*(Myy - (My*My)/line.size()) -
-                   2*ct*st*(Mxy - (Mx*My)/line.size()) +
-                   st*st*(Mxx - (Mx*Mx)/line.size());
+            return ct * ct * (Myy - (My * My) / line.size())
+                    - 2 * ct * st * (Mxy - (Mx * My) / line.size())
+                    + st * st * (Mxx - (Mx * Mx) / line.size());
         }
 
         // Get a line for drawing purposes XXX
-        public ArrayList<double[]> getLineSeg()
-        {
+        public ArrayList<double[]> getLineSeg() {
             ArrayList<double[]> lineSegment = new ArrayList<double[]>();
 
             // Calculate endpoints
             lineSegment.add(closestPoint(line.get(0)));
-            lineSegment.add(closestPoint(line.get(line.size()-1)));
+            lineSegment.add(closestPoint(line.get(line.size() - 1)));
 
             return lineSegment;
         }
 
-        public double[] getLine()
-        {
-            return new double[] {q[0], q[1], theta};
+        public double[] getLine() {
+            return new double[]{q[0], q[1], theta};
         }
 
-        public double[] closestPoint(double[] xy)
-        {
+        public ArrayList<double[]> getPoints() {
+            return line;
+        }
+
+        public double[] closestPoint(double[] xy) {
             // Calculate endpoints
             double ct = Math.cos(theta);
             double st = Math.sin(theta);
-            double[] n = LinAlg.normalize(new double[] {ct, st});
-            double[] v = LinAlg.normalize(new double[] {-st, ct});
+            double[] n = LinAlg.normalize(new double[]{ct, st});
+            double[] v = LinAlg.normalize(new double[]{-st, ct});
 
             double dotqv = LinAlg.dotProduct(q, v);
-            double[] q1 = new double[] {v[0]*dotqv, v[1]*dotqv};
+            double[] q1 = new double[]{v[0] * dotqv, v[1] * dotqv};
 
             double dotnxy = LinAlg.dotProduct(xy, n);
-            return new double[] {n[0]*dotnxy + q1[0],
-                                 n[1]*dotnxy + q1[1]};
+            return new double[]{n[0] * dotnxy + q1[0],
+                        n[1] * dotnxy + q1[1]};
         }
 
-        public double[] intersect(Line b)
-        {
+        public double[] intersect(Line b) {
             // Parallel with same theta
             if (MathUtil.doubleEquals(MathUtil.mod2pi(theta),
-                                      MathUtil.mod2pi(b.theta)))
+                    MathUtil.mod2pi(b.theta))) {
                 return null;
+            }
 
             double[] cp0 = closestPoint(new double[2]);
             double[] cp1 = b.closestPoint(new double[2]);
@@ -182,27 +170,32 @@ public class Task2 implements LCMSubscriber, ParameterListener
             double st0 = Math.sin(t0);
             double ct1 = Math.cos(t1);
             double st1 = Math.sin(t1);
-            double[][] a = new double[][] {{ct0, st0},
-                                           {ct1, st1}};
+            double[][] a = new double[][]{{ct0, st0},
+                {ct1, st1}};
             Matrix A = new Matrix(a);
-            double[] xy = A.inverse().times(new double[] {r0, r1});
+            double[] xy = A.inverse().times(new double[]{r0, r1});
             //LinAlg.print(xy);
 
             return xy;
+        }
+
+        public Line transform(double[] xyt) {
+            ArrayList<double[]> newPoints = LinAlg.transform(xyt, line);
+
+            return new Line(newPoints);
         }
     }
 
     // Get lines from a scan using agglomeration
     static public ArrayList<Line> agglomerateLines(ArrayList<double[]> points,
-                                                   double threshold,
-                                                   int maxSteps)
-    {
+            double threshold,
+            int maxSteps) {
         // Initialize tiny lines
         ArrayList<Line> lines = new ArrayList<Line>();
-        for (int i = 0; i < points.size()-1; i++) {
+        for (int i = 0; i < points.size() - 1; i++) {
             ArrayList<double[]> line = new ArrayList<double[]>();
             line.add(points.get(i));
-            line.add(points.get(i+1));
+            line.add(points.get(i + 1));
             lines.add(new Line(line));
         }
 
@@ -211,51 +204,44 @@ public class Task2 implements LCMSubscriber, ParameterListener
             Line best = null;
             double err = Double.MAX_VALUE;
             int idx = -1;
-            for (int i = 0; i < lines.size()-1; i++) {
+            for (int i = 0; i < lines.size() - 1; i++) {
                 // XXX Priority queues are the shit
-                Line temp = lines.get(i).tryLineMerge(lines.get(i+1), threshold);
+                Line temp = lines.get(i).tryLineMerge(lines.get(i + 1), threshold);
                 if (temp != null && temp.getError() < err) {
                     best = temp;
                     err = temp.getError();
                     idx = i;
                 }
             }
-            if (best == null)
+            if (best == null) {
                 break;
+            }
 
             // Remove old lines and add in new
             lines.set(idx, best);
-            lines.remove(idx+1);
+            lines.remove(idx + 1);
         }
 
         return lines;
     }
 
-    public synchronized void messageReceived(LCM lcm, String channel, LCMDataInputStream ins)
-    {
+    public synchronized void messageReceived(LCM lcm, String channel, LCMDataInputStream ins) {
         try {
             if (channel.equals("LIDAR_FRONT")) {
                 laser = new laser_t(ins);
-
-                lines = agglomerateLines(laserToPoints(laser),
-                                         pg.gd("thresh"),
-                                         pg.gi("maxsteps"));
-                // Strip out undersized lines
-                pruneLines();
-
+                agglomerateLines();
                 update();
             } else if (channel.equals("POSE")) {
                 // do nothing for Task 2
             }
-        } catch(IOException e) {
-            System.out.println("Failed to decode message on channel "+channel);
+        } catch (IOException e) {
+            System.out.println("Failed to decode message on channel " + channel);
         }
     }
 
-    private void pruneLines()
-    {
+    private void pruneLines() {
         ArrayList<Line> newLines = new ArrayList<Line>();
-        for (Line l: lines) {
+        for (Line l : lines) {
             if (l.size() > 4) {
                 newLines.add(l);
             }
@@ -263,61 +249,67 @@ public class Task2 implements LCMSubscriber, ParameterListener
         lines = newLines;
     }
 
-    public synchronized void update()
-    {
+    private void agglomerateLines() {
+        System.out.println("Thresh: " + pg.gd("thresh"));
+        lines = agglomerateLines(laserToPoints(laser),
+                pg.gd("thresh"),
+                pg.gi("maxsteps"));
+        // Strip out undersized lines
+        pruneLines();
+    }
+
+    public synchronized void update() {
         {
             VisWorld.Buffer vb = vw.getBuffer("laser-points");
             vb.addBack(new VisPoints(new VisVertexData(laserToPoints(laser)),
-                                     new VisConstantColor(Color.green),
-                                     2));
-                    vb.swap();
+                    new VisConstantColor(Color.green),
+                    2));
+            vb.swap();
         }
 
         // Draw the lines
         {
             VisWorld.Buffer vb = vw.getBuffer("lines");
-            for (Line l: lines) {
+            for (Line l : lines) {
                 ArrayList<double[]> points = l.getLineSeg();
-                if (points == null)
+                if (points == null || l.getPoints().size() < 4) {
                     continue;
+                }
                 VisConstantColor vcc = new VisConstantColor(randomColor());
                 VisVertexData vvd = new VisVertexData(points);
                 vb.addBack(new VisLines(vvd,
-                                        vcc,
-                                        1.0,
-                                        VisLines.TYPE.LINES));
+                        vcc,
+                        1.0,
+                        VisLines.TYPE.LINES));
             }
             vb.swap();
         }
     }
 
-    public void parameterChanged(ParameterGUI pg, String name)
-    {
-        if (name.equals("thresh"))
+    public void parameterChanged(ParameterGUI pg, String name) {
+        System.out.println("Parameter changed: " + name);
+        if (name.equals("thresh")) {
+            agglomerateLines();
             update();
+        }
     }
 
-    public static ArrayList<double[]> laserToPoints(laser_t laser)
-    {
+    public static ArrayList<double[]> laserToPoints(laser_t laser) {
         ArrayList<double[]> points = new ArrayList<double[]>();
         for (int i = 0; i < laser.nranges; i++) {
-            double theta = laser.rad0 + laser.radstep*i;
-            points.add(new double[] { laser.ranges[i] * Math.cos(theta),
-                                      laser.ranges[i] * Math.sin(theta) });
+            double theta = laser.rad0 + laser.radstep * i;
+            points.add(new double[]{laser.ranges[i] * Math.cos(theta),
+                        laser.ranges[i] * Math.sin(theta)});
         }
         return points;
     }
-
     static Random r = new Random();
-    public static Color randomColor()
-    {
+
+    public static Color randomColor() {
         return new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255));
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         new Task2();
     }
-
-
 }
