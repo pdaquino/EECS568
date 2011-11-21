6d82721fd361d98be1333b782737dc6cff197c33
Robert T. Goeddel <rgoeddel@umich.edu>
2011-11-20 19:15:17 -0500

    Initial voxel code. Make a build section for rgbdslam code.

---
+++ rgbd-slam/java/src/rgbdslam/RGBDSLAM.java
@@ -0,0 +1,17 @@
+package rgbdslam;
+
+import java.util.*;
+
+import april.jmat.*;
+import april.vis.*;
+
+import kinect.*;
+
+public class RGBDSLAM
+{
+
+    static public void main(String[] args)
+    {
+
+    }
+}
