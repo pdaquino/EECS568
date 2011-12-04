421caba2b9e4652c822f6e5ae202a4d12b5a5439
John Peterson <jpeterson0913@gmail.com>
2011-12-03 20:49:20 -0500

    Even more constants, will try and clean this up by migrating constants over to another file.

--- rgbd-slam/kinect/java/src/kinect/Kinect.java
+++ rgbd-slam/kinect/java/src/kinect/Kinect.java
@@ -7,11 +7,11 @@ import java.io.*;
 
 import april.jmat.*;
 
-public class Kinect
-{    
+public class Kinect {
+
     static final int WIDTH = 640;
     static final int HEIGHT = 480;
-
+    
     // John's Calibration Data
     /*
     // RGB Intrinsic Camera Parameters
@@ -22,7 +22,7 @@ public class Kinect
     // assume 0 skew
     // distortion parameters 1 2 and 5 are radial terms, 3 and 4 are tangential
     static final double[] Krgb = {0.18993, -0.52470, 0.00083, 0.00480, 0};
-
+    
     // IR Intrinsic Camera Parameters
     static final double Firx = 583.56911; // focal lengths
     static final double Firy = 582.28721; 
@@ -31,90 +31,112 @@ public class Kinect
     // assume 0 skew
     // distortion parameters 1 2 and 5 are radial terms, 3 and 4 are tangential
     static final double[] Kir = {-0.09234, 0.31571, 0.00037, -0.00425, 0};
-    */
-
+     */
+    
+    // using many more images
+    // RGB Intrinsic Camera Parameters
+    /*
+    static final double Frgbx = 523.06864; // focal lengths
+    static final double Frgby = 522.62898;
+    static final double Crgbx = 309.46501; // optial axis
+    static final double Crgby = 256.30813;
+    // assume 0 skew
+    // distortion parameters 1 2 and 5 are radial terms, 3 and 4 are tangential
+    static final double[] Krgb = {0.19117,   -0.44270,   -0.00323,   0.00064,  0.00000};
+    
+    // IR Intrinsic Camera Parameters
+    static final double Firx = 583.17822; // focal lengths
+    static final double Firy = 581.88746; 
+    static final double Cirx = 319.98857; // optical axis
+    static final double Ciry = 243.38230;
+    // assume 0 skew
+    // distortion parameters 1 2 and 5 are radial terms, 3 and 4 are tangential
+    static final double[] Kir = {-0.07415, 0.17787, 0.00168, -0.00245, 0.00000};
+     */
+    
     // Camera calibration numbers courtesy of Nicolas Burrus
     // parameters for rgb color camera
+    
     static double Frgbx = 5.2921508098293293e2; // focal length
-    static double Frgby = 5.2556393630057437e2; 
+    static double Frgby = 5.2556393630057437e2;
     static double Crgbx = 3.2894272028759258e2; // camera center in pixels
     static double Crgby = 2.6748068171871557e2;
-    static double[] Krgb = {2.6451622333009589e-1,  -8.3990749424620825e-1, 
-	-1.9922302173693159e-3, 1.4371995932897616e-3, 9.1192465078713847e-1};
-
+    static double[] Krgb = {2.6451622333009589e-1, -8.3990749424620825e-1,
+        -1.9922302173693159e-3, 1.4371995932897616e-3, 9.1192465078713847e-1};
     // parameters for IR depth camera
     static double Firx = 5.9421434211923247e2; // focal length
     static double Firy = 5.9104053696870778e2;
     static double Cirx = 3.3930780975300314e2; // camera center in pixels
     static double Ciry = 2.4273913761751615e2;
-    static double[] Kir = {-2.6386489753128833e-1, 9.9966832163729757e-1,-7.6275862143610667e-4, 
-	5.0350940090814270e-3, -1.3053628089976321};  
-   
+    static double[] Kir = {-2.6386489753128833e-1, 9.9966832163729757e-1, -7.6275862143610667e-4,
+        5.0350940090814270e-3, -1.3053628089976321};
+    
     
     // Frame buffers
     int[] rgb_buf = null;
     short[] d_buf = null;
-
     int rgb_cnt = 0;
     int d_cnt = 0;
-    
     int rgb_save_cnt = 0; // how many rgb images have we saved so far
-    int d_save_cnt = 0;  
+    int d_save_cnt = 0;
 
     // Initialize the kinect device, returning a negative
     // error code upon failure
     public native int initKinect();
+
     public native int closeKinect();
+
     public native void startVideo();
+
     public native void startRGBVideo();
+
     public native void startIRVideo();
+
     public native void stopVideo();
+
     public native void startDepth();
+
     public native void stopDepth();
+
     public native int[] getVideoFrame();
+
     public native short[] getDepthFrame();
 
-    static
-    {
-        System.loadLibrary("kinect");      
+    static {
+        System.loadLibrary("kinect");
     }
 
-    public synchronized int init()
-    {
+    public synchronized int init() {
         return initKinect();
     }
 
-    public synchronized int close()
-    {
+    public synchronized int close() {
         return closeKinect();
     }
 
-    public synchronized void start()
-    {
+    public synchronized void start() {
         startRGBVideo();
-	//startIRVideo();
+        //startIRVideo();
         startDepth();
     }
-    
+
     // same as default above
     public synchronized void startRGB() {
         startRGBVideo();
         startDepth();
     }
-    
+
     public synchronized void startIR() {
         startIRVideo();
         startDepth();
     }
 
-    public synchronized void stop()
-    {
+    public synchronized void stop() {
         stopVideo();
         stopDepth();
     }
 
-    public synchronized Frame getFrame()
-    {
+    public synchronized Frame getFrame() {
         int[] argb = getVideoFrame();
         short[] depth = getDepthFrame();
         if (argb != null) {
@@ -135,53 +157,53 @@ public class Kinect
         }
         return null;
     }
-    
+
     // rectifies distorted image from color camera using camera parameters
     public synchronized int[] rectifyRGB(int[] Dargb) {
-        
-        int[] Rargb = new int[WIDTH*HEIGHT]; // recified image
+
+        int[] Rargb = new int[WIDTH * HEIGHT]; // recified image
         // for every pixel in Rargb 
         for (int xp = 0; xp < WIDTH; xp++) {
-            double x = (xp-Crgbx)/Frgbx; // compute normalized point x
+            double x = (xp - Crgbx) / Frgbx; // compute normalized point x
             for (int yp = 0; yp < HEIGHT; yp++) {
-                double y = (yp - Crgby)/Frgby; // compute normalized point y
-                double[] XND = compXNDrgb(x,y); // apply distortion model
-                int xdp = (int) Math.floor(Frgbx*XND[0] + Crgbx); // compute pixel location
-                int ydp = (int) Math.floor(Frgby*XND[1] + Crgby); 
+                double y = (yp - Crgby) / Frgby; // compute normalized point y
+                double[] XND = compXNDrgb(x, y); // apply distortion model
+                int xdp = (int) Math.floor(Frgbx * XND[0] + Crgbx); // compute pixel location
+                int ydp = (int) Math.floor(Frgby * XND[1] + Crgby);
                 // if we have ended up outside of the image
                 if ((xdp < 0) || (xdp >= WIDTH) || (ydp < 0) || (ydp >= HEIGHT)) {
-                    Rargb[WIDTH*yp+xp] = 0xff000000; // set to black
+                    Rargb[WIDTH * yp + xp] = 0xff000000; // set to black
                 } else {
-                    Rargb[WIDTH*yp+xp] = Dargb[WIDTH*ydp+xdp];
+                    Rargb[WIDTH * yp + xp] = Dargb[WIDTH * ydp + xdp];
                 }
             }
         }
-        
-        return Rargb;  
+
+        return Rargb;
     }
-    
+
     // rectifies distored depth image using parameters from IR camera, 
     // parameters were obtained from the 640x480 so should be able to modify this image
     private synchronized short[] rectifyD(short[] Dd) {
-        
-        short[] Rd = new short[WIDTH*HEIGHT]; // rectified image
+
+        short[] Rd = new short[WIDTH * HEIGHT]; // rectified image
         for (int xp = 0; xp < WIDTH; xp++) {
-            double x = (xp-Cirx)/Firx; // compute normalized point x
+            double x = (xp - Cirx) / Firx; // compute normalized point x
             for (int yp = 0; yp < HEIGHT; yp++) {
-                double y = (yp - Ciry)/Firy; // compute normalized point y
-                double[] XND = compXNDrgb(x,y); // apply distortion model
-                int xdp = (int) Math.floor(Firx*XND[0] + Cirx); // compute pixel location
-                int ydp = (int) Math.floor(Firy*XND[1] + Ciry); 
+                double y = (yp - Ciry) / Firy; // compute normalized point y
+                double[] XND = compXNDrgb(x, y); // apply distortion model
+                int xdp = (int) Math.floor(Firx * XND[0] + Cirx); // compute pixel location
+                int ydp = (int) Math.floor(Firy * XND[1] + Ciry);
                 // if we have ended up outside of the image
                 if ((xdp < 0) || (xdp >= WIDTH) || (ydp < 0) || (ydp >= HEIGHT)) {
-                    Rd[WIDTH*yp+xp] = 2047; // set to no informatiom
+                    Rd[WIDTH * yp + xp] = 2048; // set to no informatiom
                 } else {
-                    Rd[WIDTH*yp+xp] = Dd[WIDTH*ydp+xdp];
+                    Rd[WIDTH * yp + xp] = Dd[WIDTH * ydp + xdp];
                 }
             }
         }
-        
-        return Rd;  
+
+        return Rd;
     }
 
     // given a normalized point x, y computes the normalized distorted location
@@ -190,29 +212,29 @@ public class Kinect
         // simplified expression
         //XND[0] = x;
         //XND[1] = y;
-        
+
         // full expression
-        
-        double r2 = x*x + y*y;
+
+        double r2 = x * x + y * y;
         // radial component
-        double KR = 1 + Krgb[0]*r2 + Krgb[1]*r2*r2 + Krgb[4]*r2*r2*r2;
+        double KR = 1 + Krgb[0] * r2 + Krgb[1] * r2 * r2 + Krgb[4] * r2 * r2 * r2;
         // tangential component
-        double dx = 2*Krgb[2]*x*y + Krgb[3]*(r2+2*x*x);
-        double dy = Krgb[2]*(r2+2*y*y) + 2*Krgb[3]*x*y;
-          
-        XND[0] = KR*x + dx;
-        XND[1] = KR*y + dy;
-        
+        double dx = 2 * Krgb[2] * x * y + Krgb[3] * (r2 + 2 * x * x);
+        double dy = Krgb[2] * (r2 + 2 * y * y) + 2 * Krgb[3] * x * y;
+
+        XND[0] = KR * x + dx;
+        XND[1] = KR * y + dy;
+
         return XND;
     }
-    
+
     // given a normalized point x, y computes the normalized distorted location
     private synchronized double[] compXNDdepth(double x, double y) {
         double[] XND = new double[2];
         // simplified expression
         XND[0] = x;
         XND[1] = y;
-        
+
         // full expression
         /*
         double r2 = x*x + y*y;
@@ -221,14 +243,14 @@ public class Kinect
         // tangential component
         double dx = 2*Kir[2]*x*y + Kir[3]*(r2+2*x*x);
         double dy = Kir[2]*(r2+2*y*y) + 2*Kir[3]*x*y;
-          
+        
         XND[0] = KR*x + dx;
         XND[1] = KR*y + dy;
          */
-        
+
         return XND;
     }
-    
+
     // compute the jacobian for the equations Xd = F(x,y) probably not needed
     private synchronized double[][] compJXNDrgb(double x, double y) {
         double[][] JXND = new double[2][2];
@@ -241,7 +263,7 @@ public class Kinect
         double DydDx = 0;
         // Dyd/Dy
         double DydDy = 1;
-        
+
         /* full expression neglects the 6th order radial component Krgb[5]
          * // Dxd/Dx
          * double DxdDx = 1 + 3*Krgb[1]*x*x + Krgb[1]*y*y + 5*Krgb[2]*Math.pow(x,4) + 6*Krgb[2]*y*y*x*x + Krgb[2]*Math.pow(y,4) +
@@ -254,62 +276,57 @@ public class Kinect
          * double DydDy = 1 + Krgb[1]*x*x + 3*Krgb[1]*y*y + Krgb[2]*Math.pow(x,4) + 6*Krgb[2]*y*y*x*x + 5*Krgb[2]*Math.pow(y,4) + 
          * 2*Krgb[3]*y + 4*Krgb[3]*y + 2*Krgb[4]*x;
          */
-        
+
         JXND[0][0] = DxdDx;
         JXND[0][1] = DxdDy;
         JXND[1][0] = DydDx;
         JXND[1][1] = DydDy;
-        
+
         return JXND;
     }
 
-    public synchronized void printCount()
-    {
+    public synchronized void printCount() {
         System.out.printf("rgb: %d depth: %d\n", rgb_cnt, d_cnt);
     }
-    
+
     // saves picture of RGB image to file
-    public void saveRGB(Frame frame) 
-    {
+    public void saveRGB(Frame frame) {
         BufferedImage Im = frame.makeRGB();
         try {
             File file = new File("Krgb" + Integer.toString(rgb_save_cnt) + ".jpg");
-            ImageIO.write(Im , "jpg", file);
+            ImageIO.write(Im, "jpg", file);
         } catch (IOException e) {
-            System.out.println("Failure to Save RGB!");  
+            System.out.println("Failure to Save RGB!");
         }
         rgb_save_cnt++;
     }
-    
+
     // saves depth image to file
-    public void saveD(Frame frame) 
-    {
+    public void saveD(Frame frame) {
         BufferedImage Im = frame.makeDepth();
         try {
             File file = new File("Kdepth" + Integer.toString(d_save_cnt) + ".jpg");
-            ImageIO.write(Im , "jpg", file);
+            ImageIO.write(Im, "jpg", file);
         } catch (IOException e) {
-            System.out.println("Failure to Save Depth!");  
+            System.out.println("Failure to Save Depth!");
         }
         d_save_cnt++;
     }
-    
+
     // Practical resolution of depth seems to be:
     // 632 x 480
-    static public class Frame
-    {
+    static public class Frame {
         // Not an ideal location for more constants
+
         static final public int rgbWidth = 640;
         static final public int rgbHeight = 480;
         static final public int depthWidth = 640;
         static final public int depthHeight = 480;
-
         public int[] argb;
         public short[] depth;
-
         static double[] t_gamma = null;
-        public Frame(int argb[], short[] depth)
-        {
+
+        public Frame(int argb[], short[] depth) {
             this.argb = LinAlg.copy(argb);
             this.depth = Arrays.copyOf(depth, depth.length);
 
@@ -318,7 +335,7 @@ public class Kinect
 
                 // From Daniel Shiffman
                 for (int i = 0; i < 2048; i++) {
-                    t_gamma[i] = 1.0/(i*-0.0030711016 + 3.3309495161);
+                    t_gamma[i] = 1.0 / (i * -0.0030711016 + 3.3309495161);
                 }
 
                 // From Stephane Magnenat
@@ -338,19 +355,20 @@ public class Kinect
             }
         }
 
-        public double depthToMeters(short depth)
-        {
+        
+
+        public double depthToMeters(short depth) {
             // Throw away extreme values
-            if ((int) depth >= 2048)
+            if ((int) depth >= 2048) {
                 return -1;
+            }
             return t_gamma[depth];
         }
 
-        public BufferedImage makeRGB()
-        {
-            assert (argb.length == WIDTH*HEIGHT);
+        public BufferedImage makeRGB() {
+            assert (argb.length == WIDTH * HEIGHT);
             BufferedImage im = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-            int[] buf = ((DataBufferInt)(im.getRaster().getDataBuffer())).getData();
+            int[] buf = ((DataBufferInt) (im.getRaster().getDataBuffer())).getData();
             for (int i = 0; i < buf.length; i++) {
                 buf[i] = argb[i];
             }
@@ -358,12 +376,11 @@ public class Kinect
             return im;
         }
 
-        public BufferedImage makeDepth()
-        {
-            assert (depth.length == WIDTH*HEIGHT);
+        public BufferedImage makeDepth() {
+            assert (depth.length == WIDTH * HEIGHT);
             BufferedImage im = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-            int[] buf = ((DataBufferInt)(im.getRaster().getDataBuffer())).getData();
-            double[] cutoffs = new double[] {1.0, 1.75, 2.5, 3.25, 4.0, 5.0};
+            int[] buf = ((DataBufferInt) (im.getRaster().getDataBuffer())).getData();
+            double[] cutoffs = new double[]{1.0, 1.75, 2.5, 3.25, 4.0, 5.0};
             for (int i = 0; i < buf.length; i++) {
                 // XXX Improved color mapping. Optimal range is ~0.8m - 3.5m
                 // white -> close
@@ -379,31 +396,31 @@ public class Kinect
                     buf[i] = 0;
                     continue;
                 }
-                int r,g,b;
+                int r, g, b;
                 if (m < cutoffs[0]) {
                     r = 0xff;
-                    g = 0xff - (int) (0xff * m/cutoffs[0]);
-                    b = 0xff - (int) (0xff * m/cutoffs[0]);
+                    g = 0xff - (int) (0xff * m / cutoffs[0]);
+                    b = 0xff - (int) (0xff * m / cutoffs[0]);
                 } else if (m < cutoffs[1]) {
                     r = 0xff;
-                    g = 0xff - (int) (0xff * ((cutoffs[1] - m)/(cutoffs[1]-cutoffs[0])));
+                    g = 0xff - (int) (0xff * ((cutoffs[1] - m) / (cutoffs[1] - cutoffs[0])));
                     b = 0;
                 } else if (m < cutoffs[2]) {
-                    r = (int) (0xff * ((cutoffs[2] - m)/(cutoffs[2]-cutoffs[1])));
+                    r = (int) (0xff * ((cutoffs[2] - m) / (cutoffs[2] - cutoffs[1])));
                     g = 0xff;
                     b = 0;
                 } else if (m < cutoffs[3]) {
                     r = 0;
-                    g = (int) (0xff * ((cutoffs[3] - m)/(cutoffs[3]-cutoffs[2])));
-                    b = 0xff - (int) (0xff * ((cutoffs[3] - m)/(cutoffs[3]-cutoffs[2])));
+                    g = (int) (0xff * ((cutoffs[3] - m) / (cutoffs[3] - cutoffs[2])));
+                    b = 0xff - (int) (0xff * ((cutoffs[3] - m) / (cutoffs[3] - cutoffs[2])));
                 } else if (m < cutoffs[4]) {
-                    r = 0xff - (int) (0xff * ((cutoffs[4] - m)/(cutoffs[4]-cutoffs[3])));
+                    r = 0xff - (int) (0xff * ((cutoffs[4] - m) / (cutoffs[4] - cutoffs[3])));
                     g = 0;
                     b = 0xff;
                 } else if (m < cutoffs[5]) {
-                    r = (int) (0xff * ((cutoffs[5] - m)/(cutoffs[5]-cutoffs[4])));
+                    r = (int) (0xff * ((cutoffs[5] - m) / (cutoffs[5] - cutoffs[4])));
                     g = 0;
-                    b = (int) (0xff * ((cutoffs[5] - m)/(cutoffs[5]-cutoffs[4])));
+                    b = (int) (0xff * ((cutoffs[5] - m) / (cutoffs[5] - cutoffs[4])));
                 } else {
                     r = 0;
                     g = 0;
