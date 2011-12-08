5bba45f12cc3531bf9917a0756dc7f1e9cee7fca
laurenjas@gmail.com <lauren@Pythagorus.(none)>
2011-12-08 13:31:38 -0500

    Minor changes to getTransform and camera "look at"

--- rgbd-slam/java/src/rgbdslam/RGBDSLAM.java
+++ rgbd-slam/java/src/rgbdslam/RGBDSLAM.java
@@ -40,6 +40,7 @@ public class RGBDSLAM implements LCMSubscriber
     public RGBDSLAM(GetOpt opts)
     {
         // Load from file
+        System.out.println(opts.getString("file"));
         if (opts.getString("file") != null) {
             System.out.println("Loading from file...");
             VoxelArray va = VoxelArray.readFromFile(opts.getString("file"));
@@ -206,12 +207,10 @@ public class RGBDSLAM implements LCMSubscriber
             // glitchy. Twitches and doesn't respect our axes
             DefaultCameraManager dcm = new DefaultCameraManager();
             dcm.UI_ANIMATE_MS = 25;
-            dcm.interfaceMode = 3.0;
-            double[] eye = new double[] {-10,0,0};
-            double[] lookat = new double[] {-9,0,0};
-            double[] up = new double[] {0,0,1};
-            dcm.uiLookAt(eye, lookat, up, true);
             vl.cameraManager = dcm;
+            //vl.cameraManager.setDefaultPosition(new double[] {0, 0, -20}, new double[] {0, 0, -19}, new double[] {0, -1, 0});
+            vl.cameraManager.setDefaultPosition(new double[] {-10, 0, 0}, new double[] {-9, 0, 0}, new double[] {0, 0, 1});
+            vl.cameraManager.uiDefault();
 
             jf.add(vc, BorderLayout.CENTER);
             jf.add(pg, BorderLayout.SOUTH);
@@ -232,9 +231,7 @@ public class RGBDSLAM implements LCMSubscriber
                     double[] nzaxis = LinAlg.normalize(LinAlg.subtract(cpos.lookat, cpos.eye));
                     double[] xaxis = LinAlg.normalize(LinAlg.crossProduct(nzaxis, yaxis));
 
-                    double[][] rotz = LinAlg.quatToMatrix(LinAlg.angleAxisToQuat(xyzrpy[3], nzaxis));
-                    double[][] rotx = LinAlg.quatToMatrix(LinAlg.angleAxisToQuat(xyzrpy[4], xaxis));
-                    double[][] roty = LinAlg.quatToMatrix(LinAlg.angleAxisToQuat(xyzrpy[5], yaxis));
+                    double[][] rotation = LinAlg.quatToMatrix(LinAlg.angleAxisToQuat(xyzrpy[5], yaxis));
 
                     // Translation
                     double[] eye = LinAlg.copy(cpos.eye);
@@ -245,19 +242,12 @@ public class RGBDSLAM implements LCMSubscriber
                     double[] dx = new double[] {x*xaxis[0], x*xaxis[1], x*xaxis[2]};
                     double[] dy = new double[] {y*yaxis[0], y*yaxis[1], y*yaxis[2]};
                     double[] dz = new double[] {z*nzaxis[0], z*nzaxis[1], z*nzaxis[2]};
-
                     eye = LinAlg.add(dx, LinAlg.add(dy, LinAlg.add(dz, eye)));
                     lookat = LinAlg.add(dx, LinAlg.add(dy, LinAlg.add(dz, lookat)));
 
+                    // Rotation
                     double[][] eye_trans = LinAlg.translate(eye);
                     double[][] eye_inv = LinAlg.inverse(eye_trans);
-
-                    LinAlg.timesEquals(roty, eye_inv);
-                    LinAlg.timesEquals(rotx, roty);
-                    LinAlg.timesEquals(rotz, rotx);
-                    LinAlg.timesEquals(eye_trans, rotz);
-
-                    lookat = LinAlg.transform(eye_trans, lookat);
 
                     vl.cameraManager.uiLookAt(eye, lookat, cpos.up, false);
                 }
@@ -287,16 +277,16 @@ public class RGBDSLAM implements LCMSubscriber
             if (gp == null)
                 return new double[6];
             if ((gp.buttons & 0xF0) > 1 && !turbo) {
-                vel = 5.0;
+                vel = 7.5;
                 theta_vel = Math.toRadians(45);
                 turbo = true;
-            } else if ((gp.buttons & 0xF0) == 0 && turbo) {
-                vel = 1.0;
+            } else if ((gp.buttons & 0xF0) > 1 && turbo) {
+                vel = 2.5;
                 theta_vel = Math.toRadians(15);
                 turbo = false;
             }
 
-            return new double[] {gp.axes[0]*vel*dt, gp.axes[5]*-vel*dt, gp.axes[1]*-vel*dt, gp.axes[4]*-theta_vel*dt, gp.axes[3]*-theta_vel*dt, gp.axes[2]*-theta_vel*dt};
+            return new double[] {gp.axes[0]*vel*dt, gp.axes[3]*-vel*dt, gp.axes[1]*-vel*dt, 0, 0, gp.axes[2]*-theta_vel*dt};
         }
     }
 
@@ -304,6 +294,8 @@ public class RGBDSLAM implements LCMSubscriber
     {
         Kinect.Frame currFrame = null;
         Kinect.Frame lastFrame = null;
+        ArrayList<ImageFeature> featuresL = null;
+        ArrayList<ImageFeature> featuresC = null;
 
         synchronized public void run()
         {
@@ -316,7 +308,7 @@ public class RGBDSLAM implements LCMSubscriber
                     ColorPointCloud cpc = new ColorPointCloud(currFrame);
                     VoxelArray va = new VoxelArray(DEFAULT_RES); // XXX
 
-                    // Extract features, perform RANSAC and ICP // XXX No ICP yet
+                    // Extract features, perform RANSAC
                     double[][] ransac = getTransform(cpc);
                     LinAlg.timesEquals(ransac, rbt);
                     rbt = ransac;
@@ -333,6 +325,16 @@ public class RGBDSLAM implements LCMSubscriber
                         globalVoxelFrame.merge(va, rbt);
                     }
                 }
+                else if(currFrame != null){
+                    int[] argbC = currFrame.argb;
+                    featuresC = OpenCV.extractFeatures(argbC, 640);
+                    ColorPointCloud cpc = new ColorPointCloud(currFrame);
+
+                    // Set xyz (world) coordinates
+                    for(ImageFeature fc: featuresC){
+                        fc.setXyz(cpc.Project(LinAlg.copyDoubles(fc.xy())));
+                    }
+                }
 
                 try {
                     wait();
@@ -343,23 +345,20 @@ public class RGBDSLAM implements LCMSubscriber
         synchronized public void handleFrame(Kinect.Frame frame)
         {
             lastFrame = currFrame;
+            featuresL = featuresC;
             currFrame = frame;
             notifyAll();
         }
 
         private double[][] getTransform(ColorPointCloud cpc)
         {
-            int[] argbC = currFrame.argb;
-            int[] argbL = lastFrame.argb;
-            ArrayList<ImageFeature> featuresC = OpenCV.extractFeatures(argbC, 640);
-            ArrayList<ImageFeature> featuresL = OpenCV.extractFeatures(argbL, 640);
+            assert(featuresL != null);
+
+            featuresC = OpenCV.extractFeatures(currFrame.argb, 640);
 
             // Set xyz (world) coordinates
             for(ImageFeature fc: featuresC){
                 fc.setXyz(cpc.Project(LinAlg.copyDoubles(fc.xy())));
-            }
-            for(ImageFeature fl: featuresL){
-                fl.setXyz(cpc.Project(LinAlg.copyDoubles(fl.xy())));
             }
 
             // Match SIFT features -> RANSAC
